65b92dc32aca41ab33f35533ddadf3c4b4c54a98
Matthias Friedrich <1573457+matzefriedrich@users.noreply.github.com>
2024-07-22 10:44:24 +0200

    Adds quick-start example (#1)

---
+++ examples/quick-start/cmd/internal/greeter.go
@@ -0,0 +1,21 @@
+package internal
+
+import "fmt"
+
+type Greeter interface {
+	SayHello(name string, polite bool)
+}
+
+type greeter struct{}
+
+func (g *greeter) SayHello(name string, polite bool) {
+	if polite {
+		fmt.Printf("Good day, %s!\n", name)
+	} else {
+		fmt.Printf("Hello, %s\n", name)
+	}
+}
+
+func NewGreeter() Greeter {
+	return &greeter{}
+}
