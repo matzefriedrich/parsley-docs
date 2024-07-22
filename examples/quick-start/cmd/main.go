3521a3a1b8ae52fc879306feb7dc5c72a7ce7f1c
Matthias Friedrich <1573457+matzefriedrich@users.noreply.github.com>
2024-07-22 10:19:41 +0200

    Adds quick-start example

---
+++ examples/quick-start/cmd/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+	"github.com/matzefriedrich/parsley-docs/examples/quick-start/cmd/internal"
+	"github.com/matzefriedrich/parsley/pkg/registration"
+	"github.com/matzefriedrich/parsley/pkg/resolving"
+)
+
+func main() {
+
+	registry := registration.NewServiceRegistry()
+	_ = registration.RegisterTransient(registry, internal.NewGreeter)
+
+	resolver := resolving.NewResolver(registry)
+	scope := resolving.NewScopedContext(context.Background())
+
+	greeter, _ := resolving.ResolveRequiredService[internal.Greeter](resolver, scope)
+
+	const polite = true
+	greeter.SayHello("John", polite)
+}
---
+++ examples/quick-start/go.mod
@@ -0,0 +1,7 @@
+module github.com/matzefriedrich/parsley-docs/examples/quick-start
+
+go 1.22.4
+
+toolchain go1.22.5
+
+require github.com/matzefriedrich/parsley v0.5.1
